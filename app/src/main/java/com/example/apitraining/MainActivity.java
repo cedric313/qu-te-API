104b36779350de1a8eefac8351efbfe964ca9dd0
cedric <casas.cedric@gmail.com>
2019-04-08 14:56:09 +0200

    quête aPI

---
+++ app/src/main/java/com/example/apitraining/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.apitraining;
+
+import android.content.SyncStatusObserver;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+    private final static String API_KEY = "74c3f5f64961de076b28312594173a49";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Crée une file d'attente pour les requêtes vers l'API
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        // TODO : URL de la requête vers l'API
+        String url = "https://api.openweathermap.org/data/2.5/forecast?q=Toulouse,fr&appid=" + API_KEY;
+
+        // Création de la requête vers l'API, ajout des écouteurs pour les réponses et erreurs possibles
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // TODO : traiter la réponse
+                        try {
+                            JSONArray list = response.getJSONArray("list");
+                            for(int i = 0 ; i < list.length(); i++){
+                                JSONObject object = (JSONObject) list.get(i);
+                                JSONArray weathers = (JSONArray) object.get("weather");
+                                for(int u = 0 ; u < weathers.length() ; u++){
+                                    JSONObject weather = (JSONObject) weathers.get(u);
+                                    String description = weather.getString("description");
+                                    TextView tvDescription = findViewById(R.id.tvDescription);
+                                    tvDescription.append(description + "\n");
+                                }
+
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Afficher l'erreur
+                        Log.d("VOLLEY_ERROR", "onErrorResponse: " + error.getMessage());
+                    }
+                }
+        );
+
+        // On ajoute la requête à la file d'attente
+        requestQueue.add(jsonObjectRequest);
+    }
+}
